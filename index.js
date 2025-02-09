a45d3ec7862b1b62c1ecac621269d1032d41d22f
annymous-9358 <kunikjain31@gmail.com>
2025-02-09 15:32:08 +0530

    index.js

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ const app = express();
 
 app.use(
   cors()
-  //   {
-  //   origin: (origin, callback) => {
-  //     const allowedOrigins = ['https://shopzonekunik.netlify.app'];
-  //     if (allowedOrigins.includes(origin)) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //   credentials: true
-  // }
 );
 
 app.use(express.json());
